d84d783578ccfcffc0e4df1a113e04193d90cea3
hojacky07 <hojacky.jch07@gmail.com>
2025-06-20 13:34:55 -0400

    Added player costumes and coded in animations.

--- Fracture.java
+++ Fracture.java
@@ -1,17 +1,40 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Fracture extends PApplet{
 
     Player player;
+    HashMap<String, PImage[]> playerAnimations = new HashMap<>();
+    PImage[] playerIdle = new PImage[1];
+    PImage[] playerJump = new PImage[1]; 
+    PImage[] playerAttack = new PImage[4];
+    PImage[] playerRun = new PImage[4];
 
     public void settings() {
-        size(displayWidth, displayHeight);
+        size(500, 500);
+        noSmooth();
     }
 
     public void setup() {
-        player = new Player(width / 2f, height / 2f, 50, this);
+        player = new Player(width / 2f, height / 2f, this, playerAnimations);
+
+        playerIdle[0] = loadImage("player/idle.png");
+        playerAnimations.put("idle", playerIdle);
+
+        playerJump[0] = loadImage("player/jump.png");
+        playerAnimations.put("jump", playerJump);
+
+        for (int i = 0; i < playerAttack.length; i++) {
+            playerAttack[i] = loadImage("player/attack" + (i + 1) + ".png");
+        }
+        playerAnimations.put("attack", playerAttack);
+
+        for (int i = 0; i < playerRun.length; i++) {
+            playerRun[i] = loadImage("player/run" + (i + 1) + ".png");
+        }
+        playerAnimations.put("run", playerRun);
     }
 
     public void draw() {
--- Player.java
+++ Player.java
@@ -1,17 +1,25 @@
+import java.util.HashMap;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Player {
     float x, y;
-    float size;
     float speed = 5;
-    PApplet player;
     boolean up, down, left, right;
 
-    public Player(float x, float y, float size, PApplet player) {
+    PApplet player;
+
+    HashMap<String, PImage[]> playerAnimations;
+    String currentState = "idle";
+    int frame = 0;
+    int frameDelay = 5;
+    int frameCounter = 0;
+
+    public Player(float x, float y, PApplet player, HashMap<String, PImage[]> playerAnimations) {
         this.x = x;
         this.y = y;
-        this.size = size;
         this.player = player;
+        this.playerAnimations = playerAnimations;
     }
 
     public void update() {
@@ -28,15 +36,43 @@ public class Player {
             x += speed;
         }
 
-        x = player.constrain(x, size / 2, player.width - size / 2);
-        y = player.constrain(y, size / 2, player.height - size / 2);
+        x = player.constrain(x, 19 / 2, player.width - 19 / 2);
+        y = player.constrain(y, 13 / 2, player.height - 13 / 2);
+
+        if (up || down || left || right) {
+            setState("run");
+        } else {
+            setState("idle");
+        }
+
+        PImage[] frames = playerAnimations.get(currentState);
+        if (frames.length > 1) {
+            frameCounter++;
+            if (frameCounter >= frameDelay) {
+                frameCounter = 0;
+                frame = (frame + 1) % frames.length;
+            }
+        } else {
+            frame = 0;
+        }
     }
 
     public void display() {
-        player.fill(255, 0, 0);
-        player.noStroke();
-        player.rectMode(PApplet.CENTER);
-        player.rect(x, y, size, size);
+        player.imageMode(PApplet.CENTER);
+        PImage currentFrame = playerAnimations.get(currentState)[frame];
+
+        player.pushMatrix();
+        player.translate(x, y);
+
+        if (left && !right) {
+            player.translate(-64, 0);
+            player.scale(-1, 1); 
+            player.image(currentFrame, 0, 0);
+        } else {
+            player.image(currentFrame, 0, 0);
+        }
+
+        player.popMatrix();
     }
 
     public void keyPressed(char key, int keyCode) {
@@ -96,6 +132,14 @@ public class Player {
             if (key == 'd' || key == 'D') {
                 right = false;
             }
+        }
+    }
+
+    public void setState(String state) {
+        if (!currentState.equals(state)) {
+            currentState = state;
+            frame = 0;
+            frameCounter = 0;
         }
     }
 }
